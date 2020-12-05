2eef7fb46b696ee197ae9f41521b41659c72418a
AndreiTotoro <andreitotoro@gmail.com>
2020-12-05 11:53:09 +0200

    Made the thing

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Overview from './components/Overview';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      task: '',
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  };
+
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: '',
+    });
+  };
+
+  render() {
+    const { task, tasks } = this.state;
+
+    return (
+      <div className="col-6 mx-auto mt-5">
+        <form onSubmit={this.onSubmitTask}>
+          <div className="form-group">
+            <label htmlFor="taskInput">Enter task</label>
+            <input
+              onChange={this.handleChange}
+              value={task}
+              type="text"
+              id="taskInput"
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Add Task
+            </button>
+          </div>
+        </form>
+
+        <Overview tasks={tasks} />
+      </div>
+    );
+  }
+}
+
+export default App;
